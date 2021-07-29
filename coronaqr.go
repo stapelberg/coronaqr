ff8b1d59ee901ed67fb7e490969b785d8a64a9b1
francoislpj <francoislpj@gmx.com>
2021-07-29 10:56:32 +0200

    TestRecord: add attributes TestType, TestResult and SampleDatetime (#7)

--- coronaqr.go
+++ coronaqr.go
@@ -68,27 +68,17 @@ type VaccineRecord struct {
 }
 
 type TestRecord struct {
-	Target string `cbor:"tg" json:"tg"`
-	// "tt": {
-	//   "description": "Type of Test",
-	//   "$ref": "https://id.uvci.eu/DCC.ValueSets.schema.json#/$defs/test-type"
-	// },
+	Target   string `cbor:"tg" json:"tg"`
+	TestType string `cbor:"tt" json:"tt"`
 
 	// Name is the NAA Test Name
 	Name string `cbor:"nm" json:"nm"`
 
 	// Manufacturer is the RAT Test name and manufacturer.
-	Manufacturer string `cbor:"ma" json:"ma"`
-	// "sc": {
-	//   "description": "Date/Time of Sample Collection",
-	//   "type": "string",
-	//   "format": "date-time"
-	// },
-	// "tr": {
-	//   "description": "Test Result",
-	//   "$ref": "https://id.uvci.eu/DCC.ValueSets.schema.json#/$defs/test-result"
-	// },
-	TestingCentre string `cbor:"tc" json:"tc"`
+	Manufacturer   string `cbor:"ma" json:"ma"`
+	SampleDatetime string `cbor:"sc" json:"sc"`
+	TestResult     string `cbor:"tr" json:"tr"`
+	TestingCentre  string `cbor:"tc" json:"tc"`
 	// Country of Test
 	Country       string `cbor:"co" json:"co"`
 	Issuer        string `cbor:"is" json:"is"`
